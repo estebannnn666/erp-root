33cd6c7b05fc42ab8595515016807ef8e3ad3511
estebannnn666 <estebannnn666@hotmail.com>
2018-09-24 17:25:06 -0500

    Afectar inventario al crear articulo

--- erp-modelo/src/ec/com/erp/articulo/gestor/ArticuloGestor.java
+++ erp-modelo/src/ec/com/erp/articulo/gestor/ArticuloGestor.java
@@ -1,14 +1,18 @@
 package ec.com.erp.articulo.gestor;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 
 import ec.com.erp.articulo.dao.IArticuloDAO;
 import ec.com.erp.cliente.common.exception.ERPException;
 import ec.com.erp.cliente.mdl.dto.ArticuloDTO;
+import ec.com.erp.cliente.mdl.dto.InventarioDTO;
+import ec.com.erp.inventario.gestor.IInventarioGestor;
 
 public class ArticuloGestor implements IArticuloGestor{
 
 	private IArticuloDAO articuloDAO;
+	private IInventarioGestor inventarioGestor;
 	
 	public IArticuloDAO getArticuloDAO() {
 		return articuloDAO;
@@ -16,6 +20,14 @@ public class ArticuloGestor implements IArticuloGestor{
 
 	public void setArticuloDAO(IArticuloDAO articuloDAO) {
 		this.articuloDAO = articuloDAO;
+	}
+	
+	public IInventarioGestor getInventarioGestor() {
+		return inventarioGestor;
+	}
+
+	public void setInventarioGestor(IInventarioGestor inventarioGestor) {
+		this.inventarioGestor = inventarioGestor;
 	}
 
 	/**
@@ -33,6 +45,26 @@ public class ArticuloGestor implements IArticuloGestor{
 	 * @throws ERPException
 	 */
 	public void transGuardarActualizarArticulo(ArticuloDTO articuloDTO) throws ERPException{
+		Boolean afectarInvetario = Boolean.FALSE;
+		if(articuloDTO.getId().getCodigoArticulo() ==  null){
+			afectarInvetario = Boolean.TRUE;
+		}
 		this.articuloDAO.transGuardarActualizarArticulo(articuloDTO);
+		if(afectarInvetario) {
+			InventarioDTO inventarioDTO = new InventarioDTO();
+			inventarioDTO.getId().setCodigoCompania(articuloDTO.getId().getCodigoCompania());
+			inventarioDTO.setCantidadEntrada(articuloDTO.getCantidadStock());
+			inventarioDTO.setValorUnidadEntrada(articuloDTO.getPrecio());
+			inventarioDTO.setValorTotalEntrada(BigDecimal.valueOf(Double.valueOf(""+articuloDTO.getCantidadStock())).multiply(articuloDTO.getPrecio()));
+			
+			inventarioDTO.setCantidadExistencia(inventarioDTO.getCantidadEntrada());
+			inventarioDTO.setValorUnidadExistencia(inventarioDTO.getValorUnidadEntrada());
+			inventarioDTO.setValorTotalExistencia(inventarioDTO.getValorTotalEntrada());
+			inventarioDTO.setArticuloDTO(articuloDTO);
+			inventarioDTO.setCodigoArticulo(articuloDTO.getId().getCodigoArticulo());
+			inventarioDTO.setDetalleMoviento("INVENTARIO INICIAL");
+			inventarioDTO.setUsuarioRegistro(articuloDTO.getUsuarioRegistro());
+			this.inventarioGestor.crearActualizarInventario(inventarioDTO);
+		}
 	}
 }
